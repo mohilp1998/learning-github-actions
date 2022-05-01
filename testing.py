abb0021a567ed1264a03470e82da95ddd4b95db3
mohilp1998 <mohilp1998@gmail.com>
2022-05-01 01:19:33 -0500

    Longer script

--- testing.py
+++ testing.py
@@ -12,7 +12,32 @@ cur.execute('''CREATE TABLE COMPANY
       AGE            INT     NOT NULL,
       ADDRESS        CHAR(50),
       SALARY         REAL);''')
-print("Table created successfully")
 
 conn.commit()
+print("Table created successfully")
+
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )")
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
+
+cur.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+
+print("Records created successfully")
+conn.commit()
+
+cur.execute("SELECT id, name, address, salary  from COMPANY")
+rows = cur.fetchall()
+for row in rows:
+   print ("ID = ", row[0])
+   print ("NAME = ", row[1])
+   print ("ADDRESS = ", row[2])
+   print ("SALARY = ", row[3], "\n")
+
 conn.close()
